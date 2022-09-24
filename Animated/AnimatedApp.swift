49ec6ea68e5da01b8f2c6de242a3013070dc745c
Max <mvt.2000@hotmail.com>
2022-09-24 11:51:01 -0500

    first commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Max on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated/HomeView.swift.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift.swift
+//  Animated
+//
+//  Created by Max on 28/06/22.
+//
+
+import SwiftUI
+
+struct HomeView_swift: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_swift_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView_swift()
+    }
+}
